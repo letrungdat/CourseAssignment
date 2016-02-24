6f840966054c7f84976367c6a8c5fe2925d02a76
letrungdat <dat_dragon2000@yahoo.com>
2016-02-24 17:14:33 +0700

    add angular and firebase data

---
+++ gulpfile.js
@@ -0,0 +1,76 @@
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
+var browserSync = require('browser-sync');
+var connect = require('gulp-connect');
+var reload = browserSync.reload;
+
+gulp.task('webserver', function() {
+  connect.server({
+    livereload: true
+  });
+});
+gulp.task('lint', function () {
+    return gulp.src('js/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+gulp.task('scripts', function () {
+    return gulp.src('js/*.js')
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest('dist'))
+        .pipe(rename('all.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist'));
+});
+
+
+// Rerun the task when a file changes
+gulp.task('serve', [], function () {
+    browserSync({
+        notify: false,
+        server: {
+            baseDir: '.'
+        }
+    });
+    gulp.watch(['*.html'], reload);
+    gulp.watch(['js/*.js'], reload);
+    gulp.watch(['css/*.css'], reload);
+});
+
+// Lint Task
+gulp.task('lint', function () {
+    return gulp.src('js/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+// Compile Our Sass
+gulp.task('sass', function () {
+    return gulp.src('scss/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('css'));
+});
+
+// Concatenate & Minify JS
+gulp.task('scripts', function () {
+    return gulp.src('js/*.js')
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest('dist'))
+        .pipe(rename('all.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist'));
+});
+
+// Watch Files For Changes
+gulp.task('watch', function () {
+    gulp.watch('js/*.js', ['lint', 'scripts']);
+    gulp.watch('scss/*.scss', ['sass']);
+});
+
+// Default Task
+gulp.task('default', ['lint', 'sass', 'scripts', 'watch','serve']);
