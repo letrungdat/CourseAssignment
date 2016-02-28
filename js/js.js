d98cf63079a91186941e2dbf2f9016078859b073
letrungdat <dat_dragon2000@yahoo.com>
2016-02-28 10:04:26 +0700

    update route player page

--- js/js.js
+++ js/js.js
@@ -8,11 +8,17 @@
 		        .when('/team', {
 		            templateUrl: 'template/team.html',
 		            controller: 'teamController'
+		        }).when('/table', {
+		            templateUrl: 'template/table.html',
+		            controller: 'tableController'
+		        }).when('/player/:num', {
+		            templateUrl: 'template/player.html',
+		            controller: 'playerController'
 		        }).otherwise({
 		            redirectTo: '/'
 		        });
 		}]);
-		app.controller('MyController', ['$scope', function ($scope) {
+		app.controller('MyController', ['$scope', '$firebaseArray', function ($scope, $firebaseArray) {
 		    $scope.page = {};
 		    $scope.page.title = "";
 		    $scope.menuClick = function () {
@@ -25,11 +31,22 @@
 		    $scope.news = $firebaseArray(ref.child('news'));
 		    $scope.teamCalendars = $firebaseArray(ref.child('teamCalendar'));
 	    }]);
+		app.controller('playerController', ['$scope', '$firebaseArray', '$routeParams', function ($scope, $firebaseArray, $routeParams) {
+		    $scope.page.title = "PLAYER DETAIL"
+		    var ref = new Firebase("https://cscassignment.firebaseio.com/");
+		    var number = $routeParams.num;
+		    $scope.players = $firebaseArray(ref.child('teamMembers').orderByChild('number').equalTo(parseInt(number)));
+	    }]);
 		app.controller('teamController', ['$scope', '$firebaseArray', function ($scope, $firebaseArray) {
 		    var ref = new Firebase("https://cscassignment.firebaseio.com/");
 		    $scope.page.title = "SQUAD";
-		    $scope.goalkeepers = $firebaseArray(ref.child('teamMembers').child('goalKeeper'));
-		    $scope.defenders = $firebaseArray(ref.child('teamMembers').child('Defender'));
-		    $scope.midfielders = $firebaseArray(ref.child('teamMembers').child('Midfielder'));
-		    $scope.strikers = $firebaseArray(ref.child('teamMembers').child('Striker'));
+		    $scope.goalkeepers = $firebaseArray(ref.child('teamMembers').orderByChild('type').equalTo('G'));
+		    $scope.defenders = $firebaseArray(ref.child('teamMembers').orderByChild('type').equalTo('D'));
+		    $scope.midfielders = $firebaseArray(ref.child('teamMembers').orderByChild('type').equalTo('M'));
+		    $scope.strikers = $firebaseArray(ref.child('teamMembers').orderByChild('type').equalTo('S'));
 	    }]);
+		app.controller('tableController', ['$scope', '$firebaseArray', function ($scope, $firebaseArray) {
+		    $scope.page.title = "TABLE";
+		    var ref = new Firebase("https://cscassignment.firebaseio.com/leagueTable");
+		    $scope.table = $firebaseArray(ref.orderByChild('position'));
+}]);
